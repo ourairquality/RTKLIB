4a7b571ea60aec9325595e3f120a6bbfddd64d45
Our Air Quality <info@ourairquality.org>
2024-04-09 12:07:30 +1000

    rtk_uncompress: remove unused fname

--- src/rtkcmn.c
+++ src/rtkcmn.c
@@ -3948,7 +3948,7 @@ extern int rtk_uncompress(const char *file, char *uncfile)
                 dir,fname);
 #else
         if ((p=strrchr(buff,'/'))) {
-            *p='\0'; dir=fname; fname=p+1;
+            *p='\0'; dir=fname;
         }
         sprintf(cmd,"tar -C \"%s\" -xf \"%s\"",dir,tmpfile);
 #endif
