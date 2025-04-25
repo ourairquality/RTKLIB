b94968cabac824a1d6e573e50abe36db438bf040
oaq <info@ourairquality.org>
2025-04-25 20:47:55 +1000

    SOFA: enable

--- src/rtkcmn.c
+++ src/rtkcmn.c
@@ -3992,6 +3992,7 @@ static void sunpos_eci(gtime_t tutc, const double *erpv, double *rsun) {
   char tstr[40];
   trace(4, "sunpos_eci: tutc=%s\n", time2str(tutc, tstr, 3));
 
+#define SUNPOS_SOFA
 #ifdef SUNPOS_SOFA  /* use high accuracy functions in sofa.c */
   (void)erpv;
   static THREADLOCAL gtime_t tutc_ = {0, 0};
@@ -4044,6 +4045,7 @@ static void moonpos_eci(gtime_t tutc, const double *erpv, double *rmoon) {
   char tstr[40];
   trace(4, "moonpos_eci: tutc=%s\n", time2str(tutc, tstr, 3));
 
+#define MOONPOS_SOFA
 #ifdef MOONPOS_SOFA   /* use high accuracy functions in sofa.c */
   (void)erpv;
   static THREADLOCAL gtime_t tutc_ = {0, 0};
