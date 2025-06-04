1d8d82a1cbc2c91976c8a16b05f60e5b69457ba0
oaq <info@ourairquality.org>
2025-06-04 23:22:15 +1000

    solution: support for decoding PRIDE-PPPAR and Ginan output
    Decoding the Ginan 'pos' output, and the PRIDE-PPPAR kinematic 'kin'
    output.

--- src/rtklib.h
+++ src/rtklib.h
@@ -416,6 +416,8 @@ extern "C" {
 #define SOLF_STAT   4                   /* solution format: solution status */
 #define SOLF_GSIF   5                   /* solution format: GSI F1/F2 */
 #define SOLF_PPCREF 6                   // Solution format: PPC reference pos.
+#define SOLF_GINAN  7                   // Solution format: GINAN pos.
+#define SOLF_PRIDE  8                   // Solution format: PRIDE-PPPAR kin.
 
 #define SOLQ_NONE   0                   /* solution status: no solution */
 #define SOLQ_FIX    1                   /* solution status: fix */
@@ -437,6 +439,7 @@ extern "C" {
 #define TIMES_GPST  0                   /* time system: gps time */
 #define TIMES_UTC   1                   /* time system: utc */
 #define TIMES_JST   2                   /* time system: jst */
+#define TIMES_MJDSOD 3                  /* time system: mjd sod */
 
 #define IONOOPT_OFF 0                   /* ionosphere option: correction off */
 #define IONOOPT_BRDC 1                  /* ionosphere option: broadcast model */
